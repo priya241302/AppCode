04f322c4c7e9162441ca9c9efe61bdc510b8efba
priya241302 <119650186+priya241302@users.noreply.github.com>
2023-11-17 21:36:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    10WeeksofCloudOps - TODO LIST
+                    ArgoCD App Deploy - TODO LIST
                 </Row>
   
                 <hr />
